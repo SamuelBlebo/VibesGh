ea033ba80d5aced67e005977d59fd1b90fefef80
Samuel Blebo <samuel.blebo@yahoo.com>
2020-11-20 11:58:16 +0000

    All routes and pages set up

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,33 @@ const router = express.Router()
 // @desc Landing Page 
 // @route Get /
 router.get('/', (req, res) => {
-  res.render('index.hbs')
+  res.render('index')
 })
+
+// @desc Musics
+// @route Get /music
+router.get('/music', (req, res) => {
+  res.render('music')
+})
+
+// @desc Albums
+// @route Get /music
+router.get('/albums', (req, res) => {
+  res.render('albums')
+})
+
+// @desc Videos
+// @route Get /videos
+router.get('/videos', (req, res) => {
+  res.render('videos')
+})
+
+// @desc Contact
+// @route Get /contact
+router.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
 
 
 
